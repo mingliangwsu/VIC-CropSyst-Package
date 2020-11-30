57baf01470f3b694c1162ccebe323c557e5b00ca
Liu_ Mingliang <mingliang.liu@cn121.mgmt.kamiak.wsu.edu>
2020-11-30 11:19:35 -0800

    new makefile

---
+++ compile_for_libraries.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+cd ~/dev/common/weather/build/gcc/Release
+rm *.o *.a
+make -f Makefile_Kamiak
+cd ~/dev/corn/build/gcc/Release
+rm *.o *.a
+make -f Makefile_Kamiak
+cd ~/dev/UED/library/build/gcc/Release/
+rm *.o *.a
+make -f Makefile_Kamiak
+cd ~/dev/CS_suite/build/gcc/Release/
+rm *.o *.a
+make -f Makefile_Kamiak
+cd ~/dev/VIC_CropSyst/build/library/gcc/Release/
+rm *.o *.a
+make -f Makefile_Kamiak
+
